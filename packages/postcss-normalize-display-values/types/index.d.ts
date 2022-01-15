655cb96827f2d29d5aba1b215674365e4fec0ea4
Ludovico Fischer <livrerie@gmail.com>
2022-01-15 16:49:19 +0100

    chore: improve TypeScript configuration

---
+++ packages/postcss-normalize-display-values/types/index.d.ts
@@ -0,0 +1,13 @@
+export default pluginCreator;
+declare function pluginCreator(): {
+    postcssPlugin: string;
+    prepare(): {
+        /**
+         * @param {import('postcss').Root} css
+         */
+        OnceExit(css: import('postcss').Root): void;
+    };
+};
+declare namespace pluginCreator {
+    const postcss: boolean;
+}
