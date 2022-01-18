87f78b180a3cf0b9264ae44afd9e25e8488aa644
Ludovico Fischer <livrerie@gmail.com>
2022-01-18 01:47:00 +0100

    feat(postcss-minify-params): add types wip

--- packages/postcss-minify-params/src/index.js
+++ packages/postcss-minify-params/src/index.js
@@ -6,33 +6,41 @@ import { getArguments } from 'cssnano-utils';
 /**
  * Return the greatest common divisor
  * of two numbers.
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
  */
-
 function gcd(a, b) {
   return b ? gcd(b, a % b) : a;
 }
-
+/**
+ * @param {number} a
+ * @param {number} b
+ */
 function aspectRatio(a, b) {
   const divisor = gcd(a, b);
 
   return [a / divisor, b / divisor];
 }
-
+/** @param {valueParser.Node[]} args */
 function split(args) {
   return args.map((arg) => stringify(arg)).join('');
 }
-
+/** @param {valueParser.Node} node */
 function removeNode(node) {
   node.value = '';
   node.type = 'word';
 }
-
+/** @param {unknown[]} items */
 function sortAndDedupe(items) {
   return sort([...new Set(items)], {
     insensitive: true,
   }).join();
 }
-
+/**
+ * @param {boolean} legacy
+ * @param {import('postcss').AtRule} rule */
 function transform(legacy, rule) {
   const ruleName = rule.name.toLowerCase();
 
@@ -52,10 +60,13 @@ function transform(legacy, rule) {
         node.nodes[4] &&
         node.nodes[0].value.toLowerCase().indexOf('-aspect-ratio') === 3
       ) {
-        const [a, b] = aspectRatio(node.nodes[2].value, node.nodes[4].value);
+        const [a, b] = aspectRatio(
+          Number(node.nodes[2].value),
+          Number(node.nodes[4].value)
+        );
 
-        node.nodes[2].value = a;
-        node.nodes[4].value = b;
+        node.nodes[2].value = a.toString();
+        node.nodes[4].value = b.toString();
       }
     } else if (node.type === 'space') {
       node.value = ' ';
@@ -91,11 +102,11 @@ function transform(legacy, rule) {
     rule.raws.afterName = '';
   }
 }
-
+/** @param {string} browser */
 function hasAllBug(browser) {
   return ['ie 10', 'ie 11'].includes(browser);
 }
-
+/** @param {browserslist.Options} options */
 function pluginCreator(options = {}) {
   const browsers = browserslist(null, {
     stats: options.stats,
@@ -105,7 +116,7 @@ function pluginCreator(options = {}) {
 
   return {
     postcssPlugin: 'postcss-minify-params',
-
+    /** @param {import('postcss').Root} css */
     OnceExit(css) {
       css.walkAtRules(transform.bind(null, browsers.some(hasAllBug)));
     },
