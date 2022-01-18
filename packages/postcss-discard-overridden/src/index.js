c5c21a80dc9fcb7643103069b9edd168b96ef322
Ludovico Fischer <livrerie@gmail.com>
2022-01-18 16:53:45 +0100

    feat(postcss-discard-overridden): add types

--- packages/postcss-discard-overridden/src/index.js
+++ packages/postcss-discard-overridden/src/index.js
@@ -1,28 +1,41 @@
 const OVERRIDABLE_RULES = new Set(['keyframes', 'counter-style']);
 const SCOPE_RULES = new Set(['media', 'supports']);
 
+/** @param {string} prop */
 function vendorUnprefixed(prop) {
   return prop.replace(/^-\w+-/, '');
 }
-
+/** @param {string} name */
 function isOverridable(name) {
   return OVERRIDABLE_RULES.has(vendorUnprefixed(name.toLowerCase()));
 }
-
+/** @param {string} name */
 function isScope(name) {
   return SCOPE_RULES.has(vendorUnprefixed(name.toLowerCase()));
 }
-
+/**
+ * @param {import('postcss').AtRule} node
+ * @return {string}
+ */
 function getScope(node) {
   let current = node.parent;
 
   const chain = [node.name.toLowerCase(), node.params];
 
   do {
-    if (current.type === 'atrule' && isScope(current.name)) {
-      chain.unshift(current.name + ' ' + current.params);
+    if (
+      current &&
+      current.type === 'atrule' &&
+      isScope(/** @type import('postcss').AtRule */ (current).name)
+    ) {
+      chain.unshift(
+        /** @type import('postcss').AtRule */ (current).name +
+          ' ' +
+          /** @type import('postcss').AtRule */ (current).params
+      );
     }
-    current = current.parent;
+
+    current = /** @type {any} */ (current).parent;
   } while (current);
 
   return chain.join('|');
@@ -33,9 +46,11 @@ function pluginCreator() {
     postcssPlugin: 'postcss-discard-overridden',
     prepare() {
       const cache = new Map();
+      /** @type {{node: import('postcss').AtRule, scope: string}[]} */
       const rules = [];
 
       return {
+        /** @param {import('postcss').Root} css */
         OnceExit(css) {
           css.walkAtRules((node) => {
             if (isOverridable(node.name)) {
